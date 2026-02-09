506da3b8ae398811d6714f08e2e0f0e7489e00e9
Tegegnwork <teg_law@yahoo.com>
2026-02-09 12:13:14 -0500

    Add deployment configuration

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -10,8 +10,6 @@ const auth = async (req, res, next) => {
         const token = authHeader.split(' ')[1]
         try{
                 const payload = jwt.verify(token, process.env.JWT_SECRET)
-                const user = await User.findById(payload.userId).select('-password')
-                req.user = user
                 req.user = { userId: payload.userId, name: payload.name }
                 next()
         }catch(error){
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const{login, register} = require('../controllers/auth')
 
- const{login, register} = require('../controllers/auth')
-
-  
 router.route('/login').post(login)
   router.route('/register').post(register)
 
